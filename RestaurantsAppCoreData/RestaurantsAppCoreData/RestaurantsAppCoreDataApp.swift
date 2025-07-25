76a45ab6cc7c8443e79c3da8c0aee86c086481cc
nahunp <nahun.perezv@gmail.com>
2025-07-25 16:59:23 -0400

    Initial Set Up

---
+++ RestaurantsAppCoreData/RestaurantsAppCoreData/RestaurantsAppCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantsAppCoreDataApp.swift
+//  RestaurantsAppCoreData
+//
+//  Created by CincinnatiAi Dallas on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantsAppCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
